f79cc48cc1180d9736175047f1a9890eba76554b
abdellah <abdellahedaoudi80@gmail.com>
2024-09-08 04:47:16 +0100

    origin

--- index.js
+++ index.js
@@ -79,8 +79,13 @@ server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+
+const allowedOrigins = ['https://linkerfolio.vercel.app', 'http://localhost:3000'];
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://linkerfolio.vercel.app');
+  const origin = req.headers.origin; 
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
