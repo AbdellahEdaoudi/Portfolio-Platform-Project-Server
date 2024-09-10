ab449893860a559572bc64e129985412db2adfc6
abdellah <abdellahedaoudi80@gmail.com>
2024-09-10 21:16:49 +0100

    CORS middleware

--- index.js
+++ index.js
@@ -80,12 +80,9 @@ server.listen(PORT, () => {
 });
 
 
-const allowedOrigins = ['https://linkerfolio.vercel.app', 'http://localhost:3000'];
+// CORS middleware
 app.use((req, res, next) => {
-  const origin = req.headers.origin; 
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
