8b056c0a912ae60299d07babf470cbdaa0b4afb6
abdellah <abdellahedaoudi80@gmail.com>
2024-10-19 11:11:05 +0100

    generateToken

--- generateToken.js
+++ generateToken.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const payload = { userId: 123 };
+const payload = { app: "Linkerfolio" };
 const secret = process.env.JWT_SECRET;
-const token = jwt.sign(payload, secret, { expiresIn: '1000h' });
+const token = jwt.sign(payload, secret, { expiresIn: '8760h' });
 console.log("Generated Token:", token);
